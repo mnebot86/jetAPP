b8bb1d8505fd3edd330f88f7087527fb1b6df1d0
mnebot86 <miguelnbt@gmail.com>
2023-10-26 14:09:58 -0400

    Logout
    ## Whats this for?
    Added a logout link in the setting tab. If status ok clearUser is dispatch remove currentUser and toggling isSignedIn to false. Redirecting to Login screen.
    
    ## Changes
    - in network add the api for logout
    - add logout link to setting screen
    
    ## Testing
    - [x] Checked in Compass to see if the session was destroyed.
    - [x] Redirects to Login screen

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,10 +1,36 @@
-import { Box, Text } from '@gluestack-ui/themed';
-import React from 'react';
+import { Box, Button, ButtonText, ButtonSpinner } from '@gluestack-ui/themed';
+import { logout } from 'network/auth';
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { clearUser } from 'store/slices/user';
 
 const Settings = () => {
+	const dispatch = useDispatch();
+
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleLogout = useCallback(async () => {
+		setIsLoading(true);
+
+		try {
+			const res = await logout();
+
+			if (res === 'OK') {
+				dispatch(clearUser());
+			}
+		} catch (error) {
+			throw error;
+		} finally {
+			setIsLoading(false);
+		}
+	}, [dispatch]);
+
 	return (
 		<Box flex={1} justifyContent="center" alignItems="center">
-			<Text>Settings</Text>
+			<Button variant="link" size="xl" onPress={handleLogout}>
+				<ButtonText color="$red500">Log out</ButtonText>
+				<ButtonSpinner animating={isLoading} ml={4} />
+			</Button>
 		</Box>
 	);
 };
