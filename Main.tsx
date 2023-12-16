86ce16c1eef4ff7a7595f3bfc65beafce6437a65
miguel-korio <153535561+miguel-korio@users.noreply.github.com>
2023-12-16 16:22:48 -0500

    Check Auth Intervale
    ## Whats this for?
    The session expires every 1 one. Now the verify user function run every hour. If session is over it will return the user back to login screen.
    
    ## Changes
    - In Main component - at verify is first trigger a interval will timer will start.
    
    ## Test
    - [x] App returns to login after 60 mins or is session expired.

--- Main.tsx
+++ Main.tsx
@@ -1,7 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthStack, MainStack } from 'navigation';
 import { verifyUser } from 'network/auth';
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { AppState, AppStateStatus } from 'react-native'; // Import AppState
 import { useSelector, useDispatch } from 'react-redux';
 import { getIsSignedIn } from 'store/selectors/user';
 import { setUser, clearUser } from 'store/slices/user';
@@ -10,24 +11,59 @@ import { LoginResponse } from 'utils/interface';
 const Main = () => {
 	const dispatch = useDispatch();
 	const isSignedIn = useSelector(getIsSignedIn);
+	const verifyUserInterval = useRef<NodeJS.Timeout | null>(null);
+
+	const fetchSession = useCallback(async () => {
+		try {
+			const user = (await verifyUser()) as LoginResponse;
+
+			if (user.error) {
+				dispatch(clearUser());
+			} else {
+				dispatch(setUser(user));
+			}
+		} catch (error) {
+			console.error('Error fetching user session:', error);
+		}
+	}, [dispatch]);
+
+	const scheduleRefresh = useCallback(() => {
+		const interval = 60 * 60 * 1000;
+
+		verifyUserInterval.current = setInterval(async () => {
+			await fetchSession();
+		}, interval);
+	}, [fetchSession]);
+
+	const handleAppStateChange = useCallback(
+		async (newState: AppStateStatus) => {
+			if (newState === 'active') {
+				await fetchSession();
+			}
+		},
+		[fetchSession]
+	);
 
 	useEffect(() => {
-		const fetchSession = async () => {
-			try {
-				const user = (await verifyUser()) as LoginResponse;
-
-				if (user.error) {
-					dispatch(clearUser());
-				} else {
-					dispatch(setUser(user));
-				}
-			} catch (error) {
-				throw error;
-			}
+		const initSession = async () => {
+			await fetchSession();
+			scheduleRefresh();
 		};
 
-		fetchSession();
-	}, [dispatch]);
+		initSession();
+
+		AppState.addEventListener('change', handleAppStateChange);
+
+		return () => {
+			if (verifyUserInterval.current) {
+				clearInterval(verifyUserInterval.current);
+			}
+
+			if ((AppState as any).removeEventListener) {
+				(AppState as any).removeEventListener('change', handleAppStateChange);
+			}
+		};
+	}, [dispatch, fetchSession, handleAppStateChange, scheduleRefresh]);
 
 	return <NavigationContainer>{isSignedIn ? <MainStack /> : <AuthStack />}</NavigationContainer>;
 };
